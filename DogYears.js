5e34d40adbe671d7da5b8c996c17e65216c633d2
TrevorC55 <trevor56@ksu.edu>
2022-09-08 13:06:18 -0500

    COMMIT

--- DogYears.js
+++ DogYears.js
@@ -9,11 +9,13 @@ const rl = readline.createInterface({
 });
 
 rl.question("How old are you? ", function (pyear) {
-    pyear = ((Math.log(dyear)*16)+31)
-    console.log('Your dog is ' + dyear);
+    //pyear = ((Math.log(dyear)*16)+31);
+    dyear = ((Math.pow(pyear, Math.E)/16)-31);
+    console.log(dyear);
+    //console.log('This is your age ' + pyears);
     
     rl.close();
 });
 
 
-//16ln(dog_age) + 31)
+//(human_age = 16ln(dog_age) + 31)
