ae73438b2f1d404738242093222cec47934c71f3
TrevorC55 <trevor56@ksu.edu>
2022-10-20 13:24:09 -0500

    ccmap

--- MadLibs.py
+++ MadLibs.py
@@ -15,3 +15,6 @@ def inputform():
 
 
 inputform()
+
+def inputform():
+    print(f"A vacation is when you take a trip to some {inp[0]} place with your {inp[0]} family. Usually you go to some place that is near a/an {inp[0]} or up on a/an {inp[0]} A good vacation place is one where you can ride {inp[0]} or play {inp[0]} or go hunting for" + inp[6] + ". I like to spend my time" + inp[7] + " or " + inp[8] + " When parents go on a vacation, they spend their time eating three " + inp[9] + " a day, and fathers play golf, and mothers sit around " + inp[10] + ". Last summer, my little brother fell in a/an " + inp[11] + "and got poison" + inp[12] + "all over his" + inp[13] + " My family is going to go to (the) " + inp[14] + ", and I will practice " + inp[15] + ". Parents need vacations more than kids because parents are always very " + inp[16] + "and because they have to work " + inp[17] + "hours every day all year making enough " + inp[18] + "to pay for the vacation.")
